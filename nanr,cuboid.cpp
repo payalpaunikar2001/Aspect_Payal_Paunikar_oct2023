77e51fd10ceed236e05a0c8ec58ec227295f18ec
payalpaunikar2001 <148052010+payalpaunikar2001@users.noreply.github.com>
2023-10-23 11:45:05 +0530

    Add files via upload

---
+++ nanr,cuboid.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+ 
+void s1()
+{
+	int l,b,h,res;
+	printf("enter l,b and h of cuboid\n");
+	scanf("%d %d %d",&l,&b,&h);
+	res=l*b*h;
+	printf(" volume of circle - %d",res);
+}
+main()
+{
+	s1();
+}
+
